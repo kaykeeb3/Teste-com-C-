80697051f97467b701e4ecccec8c9b8632430b9c
Kayke-Ti <104206837+Kayke-Ti@users.noreply.github.com>
2022-09-25 20:02:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Início com C# 
+{
+  class Program 
+  {
+     static void Main(string[] args)
+     {
+       // Variáveis
+       //Atribuir valor  elas
+        int numero = 10;
+        int valor_2 = 5;
+        Console.WriteLine("Quanto é" + numero + " + " + numero_2 + "?");
+       
+        int resposta = Convert.ToInt32(Console.ReadLine());
+        
+        // Estrutura condicional
+        
+        if (resposta == 15)
+        {
+          Console.WriteLine("Parabéns! você acertou!");
+        } else
+        {
+         Console.WriteLine("Tente novamente!");
+        
+     }
+  }
+  
+}
